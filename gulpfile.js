0e3316e011b0b61e41d4bea7de49e437c94396f5
defrm <99629422+defrm@users.noreply.github.com>
2023-03-11 14:25:51 +0500

    Добавил watch + comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-const {src, dest} = require('gulp');
+const {src, dest, watch} = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 
+//Настройка css
 function styles () {
     return src('app/scss/style.scss')
-    .pipe (concat('style.min.css'))
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe (concat('style.min.css'))  //concat - переименовывает файлы
+    .pipe(scss({outputStyle: 'compressed'}))  //scss(встроенная ф-ция) - минифицирует код
     .pipe(dest('app/css'))
 }
 
+//Автоматизация
+function watching () {
+    watch (['app/scss/style.scss'], styles)  //Отслиживать файл style.scss и запускать styles
+
+}
+
+//Запуск
 exports.styles = styles;
+exports.watching = watching;
